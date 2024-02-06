70542432a88eee7435d981959a8e73e9a64463ad
布卡 <106237935+bukall@users.noreply.github.com>
2024-02-07 02:41:49 +0800

    add and fix
    Add three new parameters and fix authentication error due to case of username

--- api/auth.py
+++ api/auth.py
@@ -2,6 +2,6 @@ from .config import ALLOWED_USERS
 
 
 def is_authorized(from_id: int, user_name: str) -> bool:
-    if str(user_name) in ALLOWED_USERS:
+    if str(user_name).lower() in ALLOWED_USERS:
         return True
     return False
--- api/config.py
+++ api/config.py
@@ -1,12 +1,42 @@
-import os
+﻿import os
+from re import split
+
+""" Required """
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-ALLOWED_USERS = os.getenv("ALLOWED_USERS", "").replace("@", "").split(",")
-safety_settings = {
-    "HARM_CATEGORY_SEXUALLY_EXPLICIT": "BLOCK_NONE",
-    "HARM_CATEGORY_HATE_SPEECH": "BLOCK_NONE",
-    "HARM_CATEGORY_HARASSMENT": "BLOCK_NONE",
-    "HARM_CATEGORY_DANGEROUS_CONTENT": "BLOCK_NONE",
-    "HARM_CATEGORY_SEXUAL": "BLOCK_NONE",
+GOOGLE_API_KEY = split(r'[ ,;，；]+', os.getenv("GOOGLE_API_KEY", ''))
+ALLOWED_USERS = split(r'[ ,;，；]+', os.getenv("ALLOWED_USERS", '').replace("@", "").lower())
+
+""" Optional """
+
+#Whether to push logs and enable some admin commands
+IS_DEBUG_MODE = os.getenv("IS_DEBUG_MODE", '0')
+#The target account that can execute administrator instructions and log push can use /get_my_info to obtain the ID.
+ADMIN_ID = os.getenv("ADMIN_ID", "1234567890")
+#Determines whether to verify identity. If 0, anyone can use the bot. It is enabled by default.
+AUCH_ENABLE = os.getenv("AUCH_ENABLE", "1")
+
+""" read https://ai.google.dev/api/rest/v1/GenerationConfig """
+generation_config = {
+    "max_output_tokens": 1024,
 }
+
+""" read https://ai.google.dev/api/rest/v1/HarmCategory """
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE"
+    },
+]
